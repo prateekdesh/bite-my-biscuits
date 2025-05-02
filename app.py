921139223f2fb3b630aec6f517674935b5f0102b
Prateek Deshmukh <prateek.deshmukh19@gmail.com>
2025-05-02 18:31:44 +0530

    add 1 day cookie expiry
    used unix time to add 1 day expiry for cookie, and deletion for stored cookie on expiry

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Response, Depends
 from pydantic import BaseModel
 from lethimcookie import bake, validate, sessions
+from expiry import checkExpiry, setExpiry
 
 username = "prateek"
 password = "ilovechhavi"
@@ -18,20 +19,28 @@ def login(user: User, response: Response):
     if user.username == username and user.password == password:
         session_id = bake()
         sessions.append(session_id)
+        expiry = setExpiry()
         print(sessions)
         response.set_cookie(key="Authorization", value=str(session_id), httponly=True)
+        response.set_cookie(key="Expiry", value=str(expiry), httponly=True)
         return "Logged in"
     else:
         return "Wrong username/password"
     
 @app.get("/protected")
-def protected(authorized: bool = Depends(validate)):
+def protected(response: Response, authorized: bool = Depends(validate), expired: bool = Depends(checkExpiry)):
     if not authorized:
         return "Protected! Login first"
+    
+    if expired:
+        response.delete_cookie(key="Authorization")
+        response.delete_cookie(key="Expiry")
+        return "Cookies ded lol"
     
     return "Welcome to the protected route!"
 
 @app.get("/logout")
 def logout(response: Response):
     response.delete_cookie(key="Authorization")
+    response.delete_cookie(key="Expiry")
     return "Logged out successfully"
